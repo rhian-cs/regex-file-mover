ad549b9d3116252489b1563e57d9571c47a81c19
rhian-cs <rhian.luis.cs+github@gmail.com>
2024-07-14 12:10:33 -0300

    Add basic file iteration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{error::Error, fs};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_files() {
+        eprintln!("Failure: {err}");
+    }
+}
+
+fn read_files() -> Result<(), Box<dyn Error>> {
+    for entry in fs::read_dir(".")? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            continue;
+        }
+
+        let name = entry.file_name();
+
+        println!("path = {:?}, name = {:?}", path, name);
+    }
+
+    Ok(())
 }
