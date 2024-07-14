c4d4a811e982fa44c2cd057ec1d2df440e71deb5
rhian-cs <rhian.luis.cs+github@gmail.com>
2024-07-14 12:59:48 -0300

    Create basic regex mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{error::Error, fs};
+use std::{
+    error::Error,
+    fs,
+    path::{Path, PathBuf},
+};
+
+use regex::{Captures, Regex};
 
 fn main() {
     if let Err(err) = read_files() {
@@ -7,6 +13,10 @@ fn main() {
 }
 
 fn read_files() -> Result<(), Box<dyn Error>> {
+    // let re = Regex::new(r"^[\d]{8}\.txt$").unwrap();
+    // let re = Regex::new(r"^([\d]{4})([\d]{2})([\d]{2})\.txt$").unwrap();
+    let re = Regex::new(r"^([\d]{4})([\d]{2})(01)\.txt$").unwrap();
+
     for entry in fs::read_dir(".")? {
         let entry = entry?;
         let path = entry.path();
@@ -16,9 +26,30 @@ fn read_files() -> Result<(), Box<dyn Error>> {
         }
 
         let name = entry.file_name();
+        let name = name.to_str().ok_or("Could not parse filename")?;
 
-        println!("path = {:?}, name = {:?}", path, name);
+        let mut new_path: PathBuf = match re.captures(name) {
+            Some(captures) => build_new_dir(captures),
+            None => Path::new("Uncategorized").into(),
+        };
+
+        println!("{:?} -> {:?}", path, new_path);
+
+        fs::create_dir_all(&new_path)?;
+
+        new_path.push(name);
+
+        fs::rename(path, new_path)?;
     }
 
     Ok(())
+}
+
+fn build_new_dir(captures: Captures) -> PathBuf {
+    captures
+        .iter()
+        .skip(1) // Skip whole regex match
+        .map(|cap| cap.unwrap().as_str())
+        .map(Path::new)
+        .collect()
 }
